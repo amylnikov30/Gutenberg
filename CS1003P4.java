23bb6013b8308f5630a69b73e1d277c2ef48582a
Alexander Mylnikov <am669@st-andrews.ac.uk>
2023-03-28 17:45:30 +0100

    Mess about with iteration with a window.

--- CS1003P4.java
+++ CS1003P4.java
@@ -1,14 +1,20 @@
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.apache.spark.*;
 import org.apache.spark.api.java.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CS1003P4
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
 //        Logger.getLogger("org").setLevel(Level.OFF);
 //        Logger.getLogger("akka").setLevel(Level.OFF);
+
+        final int n = 5;
 
         final SparkConf conf = new SparkConf();
         conf.setAppName("CS1003P4");
@@ -17,8 +23,20 @@ public class CS1003P4
         final JavaSparkContext ctx = new JavaSparkContext(conf);
         ctx.setLogLevel("OFF");
 
-        final JavaRDD<String> distFile = ctx.textFile("sample.txt");
+        final String content = new Scanner(new File("sample.txt")).useDelimiter("\\Z").next();
+        List<String> refined = List.of(content.replaceAll("[^a-zA-Z0-9]", " ").toLowerCase().split("[ \\s\\t\\n\\r]"));
 
-        distFile.foreach(e -> System.out.println(e));
+        final JavaRDD<String> data = ctx.parallelize(refined);
+
+        String[] window = new String[n];
+        AtomicInteger i = new AtomicInteger(0);
+        data.foreach(e ->
+        {
+            if (i.get() == n-1) i.set(0);
+            window[i.get()] = e;
+            i.set(i.get() + 1);
+
+            System.out.println(List.of(window));
+        });
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Gutenberg
 CS1003P4
+
+## Compiling
+    javac -d "<outputPath>" -cp "/cs/studres/CS1003/0-General/spark/*" *.java
+
+## Running
+For Windows replace `:` with `;`
+
+    java -cp "<outputPath>:/cs/studres/CS1003/0-General/spark/*" --add-exports java.base/sun.nio.ch=ALL-UNNAMED --add-exports java.base/sun.security.action=ALL-UNNAMED CS1003P4
+
