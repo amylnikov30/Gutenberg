2f6b7843d6df64f27bcf123900b9c567d461d5ba
Alexander Mylnikov <amylnikov30@gmail.com>
2023-03-28 16:43:13 +0100

    Add sample.txt.

--- CS1003P4.java
+++ CS1003P4.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.*;
 import org.apache.spark.api.java.*;
 
@@ -5,13 +7,18 @@ public class CS1003P4
 {
     public static void main(String[] args)
     {
+//        Logger.getLogger("org").setLevel(Level.OFF);
+//        Logger.getLogger("akka").setLevel(Level.OFF);
+
         final SparkConf conf = new SparkConf();
         conf.setAppName("CS1003P4");
-        conf.setMaster("local[3]");
+        conf.setMaster("local[*]");
 //
         final JavaSparkContext ctx = new JavaSparkContext(conf);
+        ctx.setLogLevel("OFF");
+
         final JavaRDD<String> distFile = ctx.textFile("sample.txt");
 
-        distFile.foreach(System.out::println);
+        distFile.foreach(e -> System.out.println(e));
     }
 }
