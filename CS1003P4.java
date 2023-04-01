211f9fc9948a58589b168a9ad74354c24adf8571
Alexander Mylnikov <amylnikov30@gmail.com>
2023-04-01 20:41:39 +0100

    Make a sliding window using RDDs.

--- CS1003P4.java
+++ CS1003P4.java
@@ -1,11 +1,13 @@
 import org.apache.spark.*;
 import org.apache.spark.api.java.*;
+import org.apache.spark.mllib.rdd.RDDFunctions;
+import org.apache.spark.rdd.RDD;
+import scala.reflect.ClassTag;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class CS1003P4
 {
@@ -28,15 +30,32 @@ public class CS1003P4
 
         final JavaRDD<String> data = ctx.parallelize(refined);
 
-        String[] window = new String[n];
-        AtomicInteger i = new AtomicInteger(0);
-        data.foreach(e ->
-        {
-            if (i.get() == n-1) i.set(0);
-            window[i.get()] = e;
-            i.set(i.get() + 1);
+        final ClassTag<String> STRING_CLASSTAG = scala.reflect.ClassTag$.MODULE$.apply(String.class);
 
-            System.out.println(List.of(window));
-        });
+//        SlidingRDD<String[]> sliding = new SlidingRDD<>(data.rdd(), 5, 1, STRINGARR_CLASSTAG);
+
+        RDDFunctions<String> functions = new RDDFunctions<>(data.rdd(), STRING_CLASSTAG);
+
+        // Sliding window
+        RDD<Object> slidingRdd = functions.sliding(5);
+        slidingRdd.toJavaRDD().foreach(e -> System.out.println(List.of(((String[])e))));
+
+//        RDDFunctions<String> functions = new RDDFunctions<>(data.rdd(), scala.reflect.ClassManifestFactory.fromClass(String.class));
+//        JavaRDD<String> slidingRdd = functions.sliding(5, 1).toJavaRDD().map(e -> (String)e);
+//
+//        slidingRdd.foreach(e -> System.out.println(e));
+
+//        String[] window = new String[n];
+//        AtomicInteger i = new AtomicInteger(0);
+//        data.foreach(e ->
+//        {
+//            if (i.get() == n-1) i.set(0);
+//            window[i.get()] = e;
+//            i.set(i.get() + 1);
+//
+//            System.out.println(List.of(window));
+//        });
+
+
     }
 }
