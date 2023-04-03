89d66f2cda0d105a76260b7bb228fc0ecffb2d46
Alexander Mylnikov <amylnikov30@gmail.com>
2023-04-03 20:48:38 +0100

    Distinguish between tests and actual queries

--- CS1003P4.java
+++ CS1003P4.java
@@ -1,7 +1,3 @@
-import breeze.util.Index;
-
-import java.io.FileNotFoundException;
-
 public class CS1003P4
 {
     public static void main(String[] args) throws IndexOutOfBoundsException
@@ -11,16 +7,16 @@ public class CS1003P4
             final String DATA_DIR = args[0];
             final String SEARCH_TERM = args[1];
             final float JACCARD_THRESHOLD = Float.parseFloat(args[2]);
+
+            SearchEngine.init();
+            SearchEngine.search(SEARCH_TERM, DATA_DIR, JACCARD_THRESHOLD).forEach(System.out::println);
+            SearchEngine.close();
         }
         catch (IndexOutOfBoundsException ignored)
-        {}
-
-    //    SearchEngine.init();
-    //    SearchEngine.search(SEARCH_TERM, DATA_DIR, JACCARD_THRESHOLD).forEach(System.out::println);
-    //    SearchEngine.close();
-
-        Tests.init();
-        System.out.println("All ok: " + Tests.all());
-        Tests.close();
+        {
+            Tests.init();
+            System.out.println("All ok: " + Tests.all());
+            Tests.close();
+        }
     }
 }
