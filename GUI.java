11122e6716f4fe8b3e7f33afd68c38e79405d749
Alexander Mylnikov <amylnikov30@gmail.com>
2023-04-04 20:12:24 +0100

    Slightly clean up GUI.

--- GUI.java
+++ GUI.java
@@ -12,6 +12,16 @@ public class GUI
 
     public static void init()
     {
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException |
+               IllegalAccessException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
         window = new JFrame("Search Engine");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -32,15 +42,16 @@ public class GUI
 
     public static void run(String pathToData, String searchTerm, float jaccardThreshold)
     {
-        final JPanel main = new JPanel(new FlowLayout());
-
-        final JTextField tf_searchTerm = new JTextField(searchTerm);
-        main.add(tf_searchTerm);
+        final JPanel main = new JPanel();
+        main.setLayout(new FlowLayout());
 
         final JTextField tf_pathToData = new JTextField(pathToData);
         main.add(tf_pathToData);
 
-        final JButton btn_loadPathToData = new JButton("Load game");
+        final JTextField tf_searchTerm = new JTextField(searchTerm);
+        main.add(tf_searchTerm);
+
+        final JButton btn_loadPathToData = new JButton("Choose data directory");
         btn_loadPathToData.addActionListener(e ->
         {
             JFileChooser fileChooser = new JFileChooser();
@@ -59,18 +70,21 @@ public class GUI
                 }
             }
         });
+        main.add(btn_loadPathToData);
 
         final JTextField tf_jaccardThreshold = new JTextField(Float.toString(jaccardThreshold));
         main.add(tf_jaccardThreshold);
-
-        final JLabel lb_result = new JLabel("");
-        main.add(lb_result);
 
         final JButton btn_search = new JButton("Search");
         btn_search.addActionListener(a ->
         {
             final List<String> result = SearchEngine.search(tf_pathToData.getText(), tf_searchTerm.getText(),
                     Float.parseFloat(tf_jaccardThreshold.getText()));
+
+            final JTextArea lb_result = new JTextArea("");
+            lb_result.setEditable(false);
+            main.add(lb_result);
+
             lb_result.setText("");
             result.forEach(e -> lb_result.setText(lb_result.getText() + e + ",\n"));
         });
